cc94adcbc3fdf3562e14b2ebc199f8136f82030a
归故里 <97590219+13870517674@users.noreply.github.com>
2023-01-13 23:44:48 +0800

    修改

--- src/1.7概念(concept)和约束(constraint)-创建更安全的模板.cpp
+++ src/1.7概念(concept)和约束(constraint)-创建更安全的模板.cpp
@@ -1,5 +1,37 @@
 #include<iostream>
 
+template<std::integral T>
+void f(T t){}
+
+template<class T>
+	requires std::integral<T> || std::is_pointer_v<T>
+struct X {
+
+};
+
+template<class T>
+	requires std::is_integral_v<T>
+T n{};
+
+template<class T>
+concept love = std::is_integral_v<T> && (std::is_same_v<int, T> || std::is_same_v<uint32_t, T>);
+
+void f2(love auto){}
+
 int main() {
-	puts("Hello Word");
+	f(1);
+	f('*');
+	//f(1.2);
+	X<int>x;
+	//X<double>x2;
+	X<double*>x3;
+	n<int> = 3;
+	//n<double>;
+	std::cout << n<int> << '\n';
+	f2(1);
+	f2(1u);
+	//f2(1l);
 }
+
+//Requires表达式 https://zh.cppreference.com/w/cpp/language/requires
+//约束与概念 https://zh.cppreference.com/w/cpp/language/constraints
