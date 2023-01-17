c0164b2d177a4af74635b8afdd4f7938d0b68e30
归故里 <97590219+13870517674@users.noreply.github.com>
2023-01-17 09:48:35 +0800

    结构化绑定demo完成

---
+++ src/2.3结构化绑定.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<format>
+#include<array>
+#include<tuple>
+#include<map>
+
+template < typename... Args>
+void print(const std::string_view fmt_str, Args&&... args) {
+	auto fmt_args{ std::make_format_args(args...) };
+	std::string outstr{ std::vformat(fmt_str, fmt_args) };
+	fputs(outstr.c_str(), stdout);
+}
+
+struct X { int a; double b; std::string str; };
+
+auto f()->std::tuple<int,int> {
+	return { 1,2 };
+}
+
+int main() {
+	int array[]{ 1,2,3,4,5 };
+	auto& [a, b, c, d, e] = array;
+	print("{} {} {} {} {}\n", a, b, c, d, e);
+	a = 10;
+	print("{}\n", array[0]);
+
+	std::array arr{ '*','a','b','&' };
+	auto [a_, b_, c_, d_] = arr;
+	print("{} {} {} {}\n", a_, b_, c_, d_);
+
+	std::tuple<int, double, std::string>tu{ 10,3.14,"🥵" };
+	auto [t1, t2, t3] = tu;
+	print("{} {} {}\n", t1, t2, t3);
+
+	X x{ 1,5.2,"🤣" };
+	auto [x1, x2, x3] = x;
+	print("{} {} {}\n", x1, x2, x3);
+
+	const std::array arr2{ 1,2 };
+	//auto& [c_arr1, c_arr2] = arr2;
+	//c_arr1 = 10;//error
+
+	auto [f1, f2] = f();
+	print("{} {}\n", f1, f2);
+
+	std::map<int, std::string>Map{ {1,"*"},{2,"😘"} };
+	for (const auto& [m_a, m_b] : Map) {
+		print("{} {}\n", m_a, m_b);
+	}
+}
