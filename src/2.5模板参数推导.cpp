c441f27f96369daca569b83dc6841bceeb5d0d48
归故里 <97590219+13870517674@users.noreply.github.com>
2023-01-18 19:07:17 +0800

    完成模板参数推导demo

--- src/2.5模板参数推导.cpp
+++ src/2.5模板参数推导.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
 #include"print.h"
 
-int main() {
+using namespace std::string_literals;
 
+template<class T>
+struct X {
+	T v{};
+	template<class...Args>
+	X(Args&&...args) :v{ (args + ...) } {}
+};
+
+template<class...Ts>
+X(Ts...ts) -> X<std::common_type_t<Ts...>>;
+
+int main() {
+	X x("10","🤣"s);
+	print("{}\n", x.v);
 }
