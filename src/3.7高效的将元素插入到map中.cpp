d42d33e662792f366cda6e895288fa780c95abbf
归故里 <97590219+13870517674@users.noreply.github.com>
2023-01-24 21:50:18 +0800

    完成3.7demo，标记

---
+++ src/3.7高效的将元素插入到map中.cpp
@@ -0,0 +1,29 @@
+#include"print.h"
+
+struct X {
+	std::string s;
+	X() { print("default construct\n"); }
+	X(const char* s) :s{ s } { print("construct\n"); }
+	X(const X&) { print("copy construct\n"); }
+};
+void printm(const std::map<int, X>& map) {
+	for (const auto& [k, v] : map) {
+		print("[ {}:{} ]", k, v.s);
+	}
+	print("\n");
+}
+
+int main() {
+	std::map<int, X>map{};
+	map[1] = "🐴";//两个构造的开销，有参和默认
+	print("\n");
+	//直接转发，只有一个有参构造的开销,这里使用try_emplace和emplace效果完全一样
+	map.emplace(2,"🥵");
+	map.emplace(3, "🤣");
+	printm(map);
+	print("\n");
+
+	map.emplace(1, "乐");//添加一个具有重复键的元素
+	map.try_emplace(1, "乐");
+	printm(map);
+}
