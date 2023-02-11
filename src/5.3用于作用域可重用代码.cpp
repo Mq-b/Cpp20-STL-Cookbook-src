107bdfb073215c4fce881f10b47a42be6d50e740
归故里 <97590219+13870517674@users.noreply.github.com>
2023-02-11 23:35:11 +0800

    完成5.3的demo

---
+++ src/5.3用于作用域可重用代码.cpp
@@ -0,0 +1,31 @@
+#include"print.h"
+
+int main() {
+	auto one = []() {return "one"; };
+	auto two = [] {return "two"; };
+	print("{} {}\n", one(), two());
+
+	auto p = [](auto f) {//泛型lambda，C++20之前只能使用这种方式
+		print("{}\n", f()); 
+	};
+	p([] {return "乐"; });
+
+	auto p2 = []<typename T>(T&& f) { print("{}\n", f()); };
+	p2(one);
+	p2(std::move(one));
+
+	[] <typename T>(T&& f) { print("{}\n", f()); }(two);
+
+	int num{};
+	auto p3 = [num]()mutable {num++; };
+	for (auto i = 0; i < 5; i++)p3();
+
+	print("{}\n", num);
+
+	auto p4 = [&]()mutable {num++; };
+	print("{}\n", sizeof(p4));
+
+	constexpr int n = []()constexpr {return 10 * 10; }();
+
+	auto p5 = []()->int {return 10; };
+}
