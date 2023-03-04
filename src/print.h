c770eeb18f5641d8aa412dc28a5b28c30c4274c7
归故里 <97590219+Mq-b@users.noreply.github.com>
2023-03-04 11:18:00 +0800

    修改print.h的代码，对tuple的特化，include<vector>，因为特化依赖它

--- src/print.h
+++ src/print.h
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<ranges>
 #include<map>
+#include<vector>
 
 template < typename... Args>
 void print(const std::string_view fmt_str, Args&&... args) {
@@ -59,31 +60,39 @@ concept Tuple = requires (T v) {
 
 template<Tuple T, typename CharT>
 struct std::formatter<T, CharT> {
+	using fmt_str_t = std::basic_string<CharT>;
+
 	constexpr auto parse(auto& ctx) {
-		m_fmt[m_buffer_len++] = '{';
-		auto iter = ctx.begin();
-		if (iter == ctx.end() || *iter == '}') {
-			m_fmt[m_buffer_len++] = '}';
-			return iter;
+		auto ictx = std::begin(ctx);//值得注意的是ctx并不包含前面的{或:之类的，直接就是格式字符，这也是下面用于构造string_view的原因
+		auto ectx = std::end(ctx);
+		while (true) {
+			auto rbra = std::find_if(ictx, ectx, [](auto v) {return v == '}' | v == '|'; });
+			auto viewt = std::vector<std::basic_string_view<CharT>>{ "{:", {ictx, rbra}, "}" } | std::views::join;
+			m_fmt.push_back(fmt_str_t(std::begin(viewt), std::end(viewt))); // wish for std::ranges::to
+			if (rbra != ectx && *rbra != '}') ictx = rbra + 1;
+			else { ictx = rbra;  break; }
 		}
-		m_fmt[m_buffer_len++] = ':';
-		for (; iter != ctx.end() && *iter != '}'; ++iter)
-			m_fmt[m_buffer_len++] = *iter;
-		m_fmt[m_buffer_len++] = '}';
-		return iter;
+		return ictx;
 	}
-	constexpr auto format(Tuple auto& rg, auto& ctx) {
+	constexpr auto format(Tuple auto& rg, auto& ctx) const {
+		constexpr int N = std::tuple_size_v<std::remove_reference_t<decltype(rg)>>;
 		auto iter = std::format_to(ctx.out(), "{}", '[');
-		std::basic_string_view<CharT>fmt(m_fmt, m_fmt + m_buffer_len);
-		[&] <Tuple TupleType, size_t... I>(const TupleType & rg, std::index_sequence<I...>) {
-			(..., (iter = std::vformat_to(ctx.out(), fmt, std::make_format_args(std::get<I>(rg))), iter = ' '));
-		}(rg, std::make_index_sequence< std::tuple_size_v<std::remove_reference_t<decltype(rg)>>>());
-		iter = ']';
+		auto fmt_iter = std::begin(m_fmt);
+		auto fmt_end = std::end(m_fmt);
+		const auto empty = std::string{ "{}" };
+		[&] <Tuple TupleType>(const TupleType & rg) {
+			[&] <size_t... I>(std::index_sequence<I...>) {
+				(..., (
+					iter = std::vformat_to(ctx.out(),
+						fmt_iter != fmt_end ? *(fmt_iter++) : empty, // if there is more tuple elements than fmt args, use "{}" for those missing fmt
+						std::make_format_args(std::get<I>(rg))
+					), I + 1 != N ? iter = ' ' : iter = ']'));
+			}(std::make_index_sequence<N>());
+		}(rg);
 		return iter;
 	}
 private:
-	CharT m_fmt[16]{};
-	size_t m_buffer_len = 0;
+	std::vector<fmt_str_t> m_fmt;
 };
 
 void print(std::ranges::range auto v){
